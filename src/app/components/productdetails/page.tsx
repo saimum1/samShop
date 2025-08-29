5c4f8195ca33cee0e9adf7e66d9ded78201a3761
RH Saimum <rhsaimum@RHs-MacBook-Air.local>
2025-08-29 23:16:00 +0600

    d

--- src/app/components/productdetails/page.tsx
+++ src/app/components/productdetails/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+
 import { Suspense } from "react";
 import ProductDetailPage from "./ProductDetailPage";
 
 const page = () => {
-  //  return <ProductDetailPage />
-   <Suspense fallback={<div>Loading...</div>}>
-      <ProductDetailPage />
-    </Suspense>
+   return <ProductDetailPage />
+  //  <Suspense fallback={<div>Loading...</div>}>
+  //     <ProductDetailPage />
+  //   </Suspense>
 }
 
 export default page
