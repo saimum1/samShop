bf9087a53509a1cf25973254a71b27143dbede09
RH Saimum <rhsaimum@RHs-MacBook-Air.local>
2025-08-30 15:33:02 +0600

    auth bug solved

--- src/lib/auth0.tsx
+++ src/lib/auth0.tsx
@@ -1,19 +1,4 @@
-// import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
-// export const auth0 = new Auth0Client({
-//   authorizationParameters: {
-//     scope: process.env.AUTH0_SCOPE,  // e.g., "openid profile email"
-//     audience: process.env.AUTH0_AUDIENCE,  // e.g., "https://your-api-id.auth0.com/api/v2/"
-//   },
-// });
+import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
-
-import { Auth0Client } from '@auth0/nextjs-auth0/server';
-
-export const auth0 = new Auth0Client({
-  baseURL: process.env.APP_BASE_URL,  
-  authorizationParameters: {
-    scope: process.env.AUTH0_SCOPE,  // e.g., "openid profile email"
-    // audience: process.env.AUTH0_AUDIENCE, 
-  },
-});
+export const auth0 = new Auth0Client();
